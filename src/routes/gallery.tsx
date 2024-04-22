d35bb97179d89a8b9ad9d3a7cde9c80bc28439a5
Viki Val <viktorko99@gmail.com>
2024-04-22 14:47:04 +0200

    :zap: frog app is more flexible

--- src/routes/gallery.tsx
+++ src/routes/gallery.tsx
@@ -1,31 +1,29 @@
-import { Button, Frog, parseEther } from 'frog'
-import { getCollection, getItem } from '../services/uniquery'
-import { baseTxUrl, kodaUrl } from '../utils'
 import { $purifyOne } from '@kodadot1/minipfs'
-import { CHAIN_ID, CONTRACT, HonoEnv, MINT_PRICE } from '../constants'
-import abi from '../abi.json';// with { type: 'json' }
-import { getImage } from '../services/dyndata'
+import { Button, Frog, parseEther } from 'frog'
+import abi from '../abi.json'; // with { type: 'json' }
+import { CHAIN_ID, HonoEnv, MINT_PRICE } from '../constants'
+import { getContent, getImage } from '../services/dyndata'
+import { baseTxUrl, kodaUrl } from '../utils'
 
 export const app = new Frog<HonoEnv>({})
 
+// app.frame('/:chain/:id', async (c) => {
 app.frame('/', async (c) => {
-  // const { chain, id } = { chain: 'base', id: '0x25194dfc7981d8a13367fe19b5b1c5fc010d535f' } //c.req.param()
-  // const collection = await getCollection(chain, id)
-  const collection = { name: 'UNFOLDINGS', image: 'ipfs://bafybeicdw6phpf3xyng5icxenzmxpken3izmk5gwyew3jahxrno7ql3s3e' }
-  // console.log({ collection })
+  const { chain, id } = { chain: 'base', id: '0xd9a2c93ba2e9fae10fe762a42ee807bbf95764cc' } //c.req.param()
+  // const { chain, id } = c.req.param()
+  const collection = await getContent('base', id, null)
   const image = $purifyOne(collection.image, 'kodadot_beta')
-  // const supply = collection.supply
-  // const location = kodaUrl(chain, id)
+  const price = collection.price || MINT_PRICE
 
-  const label = `${collection.name} [${MINT_PRICE} ETH]`
+  const label = `${collection.name} [${price} ETH]`
   return c.res({
     // browserLocation: location,
     title: collection.name,
     image,
-    action: '/finish',
+    action: `/finish/${id}`,
     imageAspectRatio: '1:1',
     intents: [
-      <Button.Transaction target="/mint">
+      <Button.Transaction target={`/mint/${id}`}>
         {'Mint: '}
         {label}{''}
       </Button.Transaction>,
@@ -34,27 +32,29 @@ app.frame('/', async (c) => {
   })
 })
 
-app.transaction('/mint', (c) => {
+app.transaction('/mint/:id', (c) => {
   const { address } = c
+  const { id: contractAddress } = c.req.param()
   // Contract transaction response.
   return c.contract({
     abi: abi,
     chainId: CHAIN_ID.BASE,
     functionName: 'safeMint',
     args: [address],
-    to: CONTRACT.BASE,
+    to: contractAddress as `0x${string}`,
     value: parseEther(MINT_PRICE),
   })
 })
 
-app.frame('/finish', (c) => {
+app.frame('/finish/:id', (c) => {
   const { transactionId } = c
+  const { id: contractAddress } = c.req.param()
 
   const random = Math.floor(Math.random() * 111) + 1
 
   const txUrl = transactionId ? baseTxUrl(transactionId) : undefined
-  const location = kodaUrl('base', CONTRACT.BASE)
-  const image = getImage('base', CONTRACT.BASE, String(random))
+  const location = kodaUrl('base', contractAddress)
+  const image = getImage('base', contractAddress, String(random))
   return c.res({
     browserLocation: location,
     image: image,
