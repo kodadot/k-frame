29274a10a398e7ae92359097b718b1f9c664ab58
Viki Val <viktorko99@gmail.com>
2024-04-22 14:51:02 +0200

    :broom: deno format

--- src/routes/approval.tsx
+++ src/routes/approval.tsx
@@ -1,9 +1,16 @@
 import { $purifyOne } from '@kodadot1/minipfs'
 import { Button, Frog, parseEther } from 'frog'
 import abi from '../abi.json'; // with { type: 'json' }
-import { ALTERNATIVE_CONTRACT, CHAIN, CHAIN_ID, HonoEnv, MINT_PRICE, TOKEN, TOKEN_SYMBOL } from '../constants'
+import {
+  ALTERNATIVE_CONTRACT,
+  CHAIN,
+  CHAIN_ID,
+  HonoEnv,
+  MINT_PRICE,
+  TOKEN,
+  TOKEN_SYMBOL,
+} from '../constants'
 import { getImage } from '../services/dyndata'
-import { getItem } from '../services/uniquery'
 import { baseTxUrl, kodaUrl } from '../utils'
 
 const chainId = CHAIN_ID.BASE
@@ -14,7 +21,10 @@ export const app = new Frog<HonoEnv>({})
 app.frame('/', async (c) => {
   // const { chain, id } = { chain: 'base', id: '0x25194dfc7981d8a13367fe19b5b1c5fc010d535f' } //c.req.param()
   // const collection = await getCollection(chain, id)
-  const collection = { name: 'Higher', image: 'ipfs://bafybeifb3533kgo777mzw3x3mqjodz7ico644lvemhkugdgngkdzdo7nia' }
+  const collection = {
+    name: 'Higher',
+    image: 'ipfs://bafybeifb3533kgo777mzw3x3mqjodz7ico644lvemhkugdgngkdzdo7nia',
+  }
   // console.log({ collection })
   const image = $purifyOne(collection.image, 'kodadot_beta')
   // const supply = collection.supply
@@ -28,9 +38,10 @@ app.frame('/', async (c) => {
     action: '/after',
     imageAspectRatio: '1:1',
     intents: [
-      <Button.Transaction target="/approve">
+      <Button.Transaction target='/approve'>
         {'Approve: '}
-        {label}{''}
+        {label}
+        {''}
       </Button.Transaction>,
       // <Button.Link href={location}>View</Button.Link>,
     ],
@@ -62,15 +73,13 @@ app.frame('/after', (c) => {
     imageAspectRatio: '1:1',
     action: '/finish',
     intents: [
-      <Button.Transaction target="/mint">
-      {'Mint: Higher'}
-    </Button.Transaction>,
-      txUrl ? <Button.Link href={txUrl}>View approval TX</Button.Link>: null,
-    ]
+      <Button.Transaction target='/mint'>
+        {'Mint: Higher'}
+      </Button.Transaction>,
+      txUrl ? <Button.Link href={txUrl}>View approval TX</Button.Link> : null,
+    ],
   })
 })
-
-
 
 app.transaction('/mint', (c) => {
   const { address } = c
@@ -98,56 +107,13 @@ app.frame('/finish', (c) => {
     image: image,
     imageAspectRatio: '1:1',
     intents: [
-      txUrl ? <Button.Link href={txUrl}>View TX</Button.Link>: null,
-      location ? <Button.Link href={location}>Collection view</Button.Link>: null,
-    ]
-  })
-})
-
-//:curr represents the current item id while :id represents the collection (e.g 106)
-app.frame('/view/:chain/:id/:curr', async (c) => {
-  let { chain, id, curr } = c.req.param()
-  const { buttonValue } = c
-
-  // There is no max defined
-  if (!buttonValue) {
-    throw new Error('The collection should have a maximum')
-  }
-  let max = Number(buttonValue)
-  let item = await getItem(chain, id, curr)
-
-  if (!item) {
-    curr = '1'
-    item = await getItem(chain, id, curr)
-  }
-
-  const image = $purifyOne(item.image, 'kodadot_beta')
-
-  const random = max ? Math.floor(Math.random() * max) + 1 : curr + 1
-
-  return c.res({
-    image: image,
-    imageAspectRatio: '1:1',
-    intents: [
-      parseInt(curr) > 1 ? (
-        <Button value={`${max}`} action={`/view/${chain}/${id}/${parseInt(curr) - 1}/`}>
-          {' '}
-          ←{' '}
-        </Button>
-      ) : null,
-      <Button value={`${max}`} action={`/view/${chain}/${id}/${parseInt(curr) + 1}/`}>
-        {' '}
-        →{' '}
-      </Button>,
-
-      <Button action={`/view/${chain}/${id}/${random}`} value={`${max}`}>
-        {' '}
-        ↻{' '}
-      </Button>,
-      <Button.Link href={kodaUrl(chain, id, curr)}>View</Button.Link>,
+      txUrl ? <Button.Link href={txUrl}>View TX</Button.Link> : null,
+      location
+        ? <Button.Link href={location}>Collection view</Button.Link>
+        : null,
     ],
-    browserLocation: kodaUrl(chain, id, curr),
   })
 })
+
 
 export default app
--- src/services/dyndata.ts
+++ src/services/dyndata.ts
@@ -1,9 +1,15 @@
 const BASE_URL = 'https://dyndata.deno.dev/'
 
-export async function getContent(chain: string, collection: string, id: string | null) {
+export async function getContent(
+  chain: string,
+  collection: string,
+  id: string | null,
+) {
   try {
     const possibleId = id ? `/${id}` : ''
-    const result = await fetch(`${BASE_URL}${chain}/content/${collection}` + possibleId)
+    const result = await fetch(
+      `${BASE_URL}${chain}/content/${collection}` + possibleId,
+    )
     console.log(result)
     return await result.json()
   } catch (error) {
@@ -11,7 +17,6 @@ export async function getContent(chain: string, collection: string, id: string |
     return null
   }
 }
-
 
 export function getImage(chain: string, collection: string, id: string) {
   return `${BASE_URL}${chain}/image/${collection}/${id}`
