aed649b16822d9b03bb933c46f278248914019ad
Viki Val <viktorko99@gmail.com>
2024-04-22 14:49:38 +0200

    :broom: denofmt

--- src/routes/gallery.tsx
+++ src/routes/gallery.tsx
@@ -1,6 +1,6 @@
 import { $purifyOne } from '@kodadot1/minipfs'
 import { Button, Frog, parseEther } from 'frog'
-import abi from '../abi.json'; // with { type: 'json' }
+import abi from '../abi.json' // with { type: 'json' }
 import { CHAIN_ID, HonoEnv, MINT_PRICE } from '../constants'
 import { getContent, getImage } from '../services/dyndata'
 import { baseTxUrl, kodaUrl } from '../utils'
@@ -9,7 +9,10 @@ export const app = new Frog<HonoEnv>({})
 
 // app.frame('/:chain/:id', async (c) => {
 app.frame('/', async (c) => {
-  const { chain, id } = { chain: 'base', id: '0xd9a2c93ba2e9fae10fe762a42ee807bbf95764cc' } //c.req.param()
+  const { chain, id } = {
+    chain: 'base',
+    id: '0xd9a2c93ba2e9fae10fe762a42ee807bbf95764cc',
+  } //c.req.param()
   // const { chain, id } = c.req.param()
   const collection = await getContent('base', id, null)
   const image = $purifyOne(collection.image, 'kodadot_beta')
@@ -25,7 +28,8 @@ app.frame('/', async (c) => {
     intents: [
       <Button.Transaction target={`/mint/${id}`}>
         {'Mint: '}
-        {label}{''}
+        {label}
+        {''}
       </Button.Transaction>,
       // <Button.Link href={location}>View</Button.Link>,
     ],
@@ -60,9 +64,11 @@ app.frame('/finish/:id', (c) => {
     image: image,
     imageAspectRatio: '1:1',
     intents: [
-      txUrl ? <Button.Link href={txUrl}>View TX</Button.Link>: null,
-      location ? <Button.Link href={location}>Collection view</Button.Link>: null,
-    ]
+      txUrl ? <Button.Link href={txUrl}>View TX</Button.Link> : null,
+      location
+        ? <Button.Link href={location}>Collection view</Button.Link>
+        : null,
+    ],
   })
 })
 
